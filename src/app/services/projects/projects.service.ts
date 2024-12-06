2b8ac049c6812c43edebb298c05c71cb2d1db29e
Alejandro <aleig.906@gmail.com>
2024-12-05 23:25:07 -0300

    ad

--- src/app/services/projects/projects.service.ts
+++ src/app/services/projects/projects.service.ts
@@ -12,51 +12,96 @@ export class ProjectsService {
   private projects: Project[] = [
     {
       id: 0,
-      name: 'Auth Forms',
+      name: 'AuthFlow',
       description: 'ANGULAR_FORMS_DESC',
       link: 'https://angular-auth-forms.netlify.app/',
       image: 'https://i.postimg.cc/HLfBr6Gh/actdel.png',
       showDetails: false,
-      technologies: [
-        {
-          name: 'angular',
-          image:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbOFmjGchTMwQriXqezOovYKqXWK3YXUnFlQ&s',
-        },
-        {
-          name: 'material-design',
-          image:
-            'https://www.evernote.design/assets/images/material-design-system.jpg',
-        },
-        {
-          name: 'angular-material',
-          image: 'https://image.pngaaa.com/160/4144160-middle.png',
-        },
-        {
-          name: 'prime-ng',
-          image:
-            'https://external-preview.redd.it/4JkXZgPF9IW3_LUNCxSGPRE388KpNXIGgS6r6uFS1EY.jpg?auto=webp&s=b369ca8a19c5570e9a11e6455a71e0b83a6bf54a',
-        },
-        {
-          name: 'bootstrap',
-          image:
-            'https://e7.pngegg.com/pngimages/439/345/png-clipart-bootstrap-logo-thumbnail-tech-companies.png',
-        },
-        {
-          name: 'html',
-          image:
-            'https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png?f=webp&w=256',
-        },
-        {
-          name: 'sass',
-          image: 'https://sass-lang.com/assets/img/styleguide/seal-color.png',
-        },
-        {
-          name: 'typescript',
-          image:
-            'https://typescript-eslint.io/assets/files/logo-62ab572de114d03f1ec685d989f92cd6.svg',
-        },
-      ],
+      clientSide: {
+        projectName: 'E-Commerce',
+        side: 'Frontend',
+        description: 'AUTH_CLIENT_DESC',
+        linkRepository: 'https://github.com/Chiquitolina/authForms',
+        technologies: [
+          {
+            name: 'angular',
+            image:
+              'https://seeklogo.com/images/A/angular-icon-logo-5FC0C40EAC-seeklogo.com.png',
+          },
+          {
+            name: 'material-design',
+            image:
+              'https://www.evernote.design/assets/images/material-design-system.jpg',
+          },
+          {
+            name: 'angular-material',
+            image: 'https://image.pngaaa.com/160/4144160-middle.png',
+          },
+          {
+            name: 'prime-ng',
+            image:
+              'https://external-preview.redd.it/4JkXZgPF9IW3_LUNCxSGPRE388KpNXIGgS6r6uFS1EY.jpg?auto=webp&s=b369ca8a19c5570e9a11e6455a71e0b83a6bf54a',
+          },
+          {
+            name: 'bootstrap',
+            image:
+              'https://e7.pngegg.com/pngimages/439/345/png-clipart-bootstrap-logo-thumbnail-tech-companies.png',
+          },
+          {
+            name: 'html',
+            image:
+              'https://cdn.iconscout.com/icon/free/png-256/free-html-5-1-1175208.png?f=webp&w=256',
+          },
+          {
+            name: 'sass',
+            image: 'https://sass-lang.com/assets/img/styleguide/seal-color.png',
+          },
+          {
+            name: 'typescript',
+            image:
+              'https://typescript-eslint.io/assets/files/logo-62ab572de114d03f1ec685d989f92cd6.svg',
+          },
+        ],
+      },
+      serverSide: {
+        projectName: 'E-Commerce',
+        side: 'Backend',
+        description: 'AUTH_SERVER_DESC',
+        linkRepository: 'https://github.com/Chiquitolina/auth-node-express-prisma',
+        technologies: [
+          {
+            name: 'node-js',
+            image:
+              'https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png',
+          },
+          {
+            name: 'express-js',
+            image: 'https://img.icons8.com/color/512/express-js.png',
+          },
+          {
+            name: 'mvc-pattern',
+            image: 'https://static.thenounproject.com/png/4199124-200.png',
+          },
+          {
+            name: 'prisma',
+            image: 'https://img.icons8.com/fluency/154/prisma-orm.png',
+          },
+          {
+            name: 'zod',
+            image: 'https://raw.githubusercontent.com/colinhacks/zod/master/logo.svg?sanitize=true',
+          },
+          {
+            name: 'typescript',
+            image:
+              'https://typescript-eslint.io/assets/files/logo-62ab572de114d03f1ec685d989f92cd6.svg',
+          },
+          {
+            name: 'mysql',
+            image:
+              'https://pngimg.com/uploads/mysql/mysql_PNG23.png',
+          },
+        ],
+      },
     },
     {
       id: 1,
